34b9ea00940a7fabf4f4f122094838569c4f4c49
rlwjd31 <rlwjd31@naver.com>
2023-11-25 00:03:32 +0900

    🚀[feat]: react-query 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import App from "./App.tsx";
+import "./index.css";
+
+const queryClient = new QueryClient();
+const isDevelopmentMode = import.meta.env.MODE === "development";
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+      {isDevelopmentMode && <ReactQueryDevtools />}
+    </QueryClientProvider>
+  </React.StrictMode>
+);
