78f88a05e301cce289d179cc32fdc38436847ede
rlwjd31 <rlwjd31@naver.com>
2023-11-25 00:09:34 +0900

    💄[Style]: 영화 컴포넌트 style -> flex

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>infinite scroll with react-query</h1>
+
+      <div className="container">
+        <div style={{ backgroundColor: "red" }} className="movie-card">
+          div1
+        </div>
+        <div style={{ backgroundColor: "blue" }} className="movie-card">
+          div1
+        </div>
+        <div style={{ backgroundColor: "green" }} className="movie-card">
+          div1
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
