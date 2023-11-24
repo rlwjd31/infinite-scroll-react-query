9f2ab363761477af0a2f00ccbad56e7c15d9b744
rlwjd31 <rlwjd31@naver.com>
2023-11-25 01:47:53 +0900

    🚀[Feat]: Movie 컴포넌트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
+import { fetchMovies } from "./api/fetchMovie";
 
 import "./App.css";
-import { fetchMovies } from "./api/fetchMovie";
+import { Movie as TMovie } from "./types/movie";
+import Movie from "./Movie";
 
 function App() {
   const { data, status, error, hasNextPage, fetchNextPage } = useInfiniteQuery({
@@ -14,20 +16,17 @@ function App() {
   if (status === "error") return <>{`fail to fetch data ${error.message}`}</>;
   if (status === "pending") return <>Loading...</>;
 
+  // status === "success"일 때로 data가 있음을 보장.
+  const movies = data?.pages.map((pageMovieData: TMovie[]) =>
+    pageMovieData.map((movieContent: TMovie) => (
+      <Movie key={movieContent.id} movie={movieContent} />
+    ))
+  );
+
   return (
     <>
       <h1>infinite scroll with react-query</h1>
-      <div className="container">
-        <div style={{ backgroundColor: "red" }} className="movie-card">
-          div1
-        </div>
-        <div style={{ backgroundColor: "blue" }} className="movie-card">
-          div1
-        </div>
-        <div style={{ backgroundColor: "green" }} className="movie-card">
-          div1
-        </div>
-      </div>
+      <div className="container">{movies}</div>
     </>
   );
 }
