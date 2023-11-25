7710677a4d573412221f8b8373673caec650820a
rlwjd31 <rlwjd31@naver.com>
2023-11-25 21:32:17 +0900

    🚀[Feat]: 무한스크롤 구현
    - 가장 밑 screen에 observer의 관찰자 대상인 div element추가
    - 테스트 버튼 요소(trigger fetchNextPage) 제거
    - observer intersection이용

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,42 @@ import { fetchMovies } from "./api/fetchMovie";
 import "./App.css";
 import { Movie as TMovie } from "./types/movie";
 import Movie from "./Movie";
+import { useEffect, useRef } from "react";
 
 const MAX_PAGE = 3;
 
 function App() {
-  const { data, status, error, hasNextPage, fetchNextPage } = useInfiniteQuery({
+  const { data, status, error, fetchNextPage, isLoading } = useInfiniteQuery({
     queryKey: ["movies"],
     queryFn: fetchMovies,
     initialPageParam: 1,
     getNextPageParam: (lastPage, allPages, lastPageParam, allPageParams) => {
-      console.log(
-        "props in getNextPageParam",
-        lastPage,
-        allPages,
-        lastPageParam,
-        allPageParams
-      );
-
       return lastPageParam + 1 > MAX_PAGE ? null : lastPageParam + 1;
     },
   });
+  const targetElement = useRef<HTMLDivElement>(null);
+  const observerCallback = (entries: IntersectionObserverEntry[]) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log("target과 intersection 됨.");
+        fetchNextPage();
+      }
+    });
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(observerCallback, {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0,
+    });
+
+    if (targetElement.current) observer.observe(targetElement.current);
+
+    return () => {
+      targetElement.current && observer.unobserve(targetElement.current);
+    };
+  }, []);
 
   if (status === "error") return <>{`fail to fetch data ${error.message}`}</>;
   if (status === "pending") return <>Loading...</>;
@@ -36,13 +52,18 @@ function App() {
   );
 
   return (
-    <>
+    <div className="margin-auto">
       <h1>infinite scroll with react-query</h1>
-      <button disabled={!hasNextPage} onClick={() => fetchNextPage()}>
-        get Next Page
-      </button>
       <div className="container">{movies}</div>
-    </>
+      <div
+        style={{ height: "200px", backgroundColor: "red", fontSize: "3rem" }}
+        ref={targetElement}
+      >
+        {isLoading
+          ? "loading next page"
+          : "intersection with this area so fetch next page will be invoke!"}
+      </div>
+    </div>
   );
 }
 
